45da58dd296486a390956619803390e4d9f0fb6d
Javier Ibanez <Expertprep2015@gmail.com>
2023-07-20 00:00:06 -0400

    added imports

--- app.py
+++ app.py
@@ -3,16 +3,46 @@ import smtplib
 from email.message import EmailMessage
 import os
 from google.oauth2 import service_account
+import gspread
+import pandas as pd
 
 app = Flask(__name__)
 
 # Path to your credential JSON file
-credential_file = os.path.join(os.getcwd(), 'credentials/your_service_account_key.json')
+credential_file = os.path.join(os.getcwd(), 'credentials/credentials/spartan-soccer-0861efb1932a.json')
 
 # Load credentials from the JSON file
 credentials = service_account.Credentials.from_service_account_file(
     credential_file, scopes=['https://www.googleapis.com/auth/spreadsheets.readonly']
 )
+
+# Create a client to interact with Google Sheets API
+client = gspread.authorize(credentials)
+
+# Open the Google Spreadsheet
+spreadsheet = client.open('Your Spreadsheet Name')
+
+# Select the specific sheet within the spreadsheet
+sheet = spreadsheet.worksheet('Sheet1')  # Replace 'Sheet1' with your actual sheet name
+
+# Read the data from the sheet into a pandas DataFrame
+data = sheet.get_all_values()
+headers = data[0]
+rows = data[1:]
+df = pd.DataFrame(rows, columns=headers)
+
+# Process the DataFrame to generate player cards dynamically
+for index, row in df.iterrows():
+    player_name = row['Player Name']
+    goals = row['Goals']
+    shots = row['Shots']
+    assists = row['Assists']
+    matches_played = row['Matches Played']
+
+    # Generate player card dynamically using the retrieved data
+    # Your code here to generate and display player cards
+
+# Rest of your Flask application code
 
 @app.route("/players")
 def players():
